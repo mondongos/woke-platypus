9d7cb2c939fa06e7ca7407646ac7525a6ad4bd01
Max Van Aken <mtvanaken@gmail.com>
2019-07-11 15:18:51 +0100

    Answer pages mapped to question page

--- app/src/main/java/com/platypus/woke/Activity2.java
+++ app/src/main/java/com/platypus/woke/Activity2.java
@@ -1,14 +1,53 @@
 package com.platypus.woke;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity2 extends AppCompatActivity {
+    private Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        button1 = (Button) findViewById(R.id.answerA);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity4();
+
+            }
+        });
+        button2 = (Button) findViewById(R.id.answerB);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+
+            }
+        });
+        button3= (Button) findViewById(R.id.answerC);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity4();
+
+            }
+        });
+
+    }
+
+    public void openActivity3(){
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+
+    public void openActivity4(){
+        Intent intent = new Intent(this, Activity4.class);
+        startActivity(intent);
     }
 }
